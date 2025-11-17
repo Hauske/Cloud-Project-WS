ec80fd57c3d68098e79dac7678234c5b3e9b884c
Isaac Noriega <isaac.noriega@iteso.mx>
2025-11-17 14:45:34 -0600

    Add CV CRUD controller and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
+import cvRoutes from "./cvRoutes";
 
 const router = Router();
 router.use("/users", userRoutes);
+router.use("/cvs", cvRoutes);
 
 
 export default router;
