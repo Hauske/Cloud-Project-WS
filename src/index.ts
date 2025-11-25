bb4ff4595f389e4f2e8e9e960777fdbfb7e39c51
Isaac Noriega <isaac.noriega@iteso.mx>
2025-11-25 14:42:03 -0600

    ci: pipeline y docker para backend

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
